d3e9a9aafba6b4f2fb667bd029da90e776a81c7f
ashok898 <ashokkumarkota1999@gmail.com>
2025-11-21 15:53:48 +0530

    Add Jenkins setup script for Amazon Linux
    This script sets up Jenkins on Amazon Linux, installs Java 21, and configures the Jenkins service to use the installed Java version.

---
+++ jekinssetup-in -amazonlinux.sh
@@ -0,0 +1,43 @@
+# --- Update system ---
+sudo yum update -y
+
+# --- Install prerequisites ---
+sudo yum install -y wget git
+
+# --- Install Java 21 (Adoptium tarball, since Amazon Linux repos don’t have it yet) ---
+cd /opt
+wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz
+tar -xvzf OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz
+ln -sfn jdk-21.0.5+11 jdk-21
+
+# Register Java
+sudo alternatives --install /usr/bin/java java /opt/jdk-21/bin/java 1
+sudo alternatives --install /usr/bin/javac javac /opt/jdk-21/bin/javac 1
+sudo alternatives --set java /opt/jdk-21/bin/java
+sudo alternatives --set javac /opt/jdk-21/bin/javac
+
+# Verify Java
+java -version
+
+# --- Install Jenkins ---
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install -y jenkins
+
+# --- Configure Jenkins service to use Java 21 ---
+sudo systemctl edit jenkins
+# [Service]
+# Environment="JAVA_HOME=/opt/jdk-21"
+# ExecStart=
+# ExecStart=/opt/jdk-21/bin/java -Djava.awt.headless=true -jar /usr/share/java/jenkins.war --httpPort=8080 --webroot=/var/cache/jenkins/war
+
+sudo systemctl daemon-reload
+
+# --- Start Jenkins ---
+sudo systemctl reset-failed jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+
+# --- Get initial admin password ---
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
