e64abb68c4e9e07b76cd2586bd00fba731095029
ashok898 <ashokkumarkota1999@gmail.com>
2025-11-21 16:16:47 +0530

    Update Maven download source to archive

--- maven-in-rhell.sh
+++ maven-in-rhell.sh
@@ -5,15 +5,16 @@ sudo yum update -y
 sudo yum install -y wget tar
 
 # --- Download latest Maven (example: 3.9.9) ---
+# --- Download Maven from archive ---
 cd /opt
-wget https://downloads.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+wget https://archive.apache.org/dist/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
 
 # --- Extract and set stable path ---
 tar -xvzf apache-maven-3.9.9-bin.tar.gz
 ln -sfn apache-maven-3.9.9 maven
 
 # --- Configure environment variables ---
-echo 'export M2_HOME=/opt/maven' | sudo tee -a /etc/profile.d/maven.sh
+echo 'export M2_HOME=/opt/maven' | sudo tee /etc/profile.d/maven.sh
 echo 'export PATH=$M2_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 
