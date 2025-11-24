058a8dc012deb3f10db28c8a2d83c2658d439929
ashok898 <ashokkumarkota1999@gmail.com>
2025-11-24 23:38:14 +0530

    Update script to install Docker on RHEL
    Added steps to update the system and install Docker Engine.

--- dokcer-in-rhel.sh
+++ dokcer-in-rhel.sh
@@ -21,3 +21,26 @@ docker --version
 
 # --- Run test container ---
 sudo docker run hello-world
+
+++++++++++++++++++++++++++++++++++++++++++++++++or++++++++++++++++++++++++++++++++++++++++++++++++
+
+ # --- Update system ---
+   59  sudo yum update -y
+   60  # --- Remove old versions if any ---
+   61  sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+   62  # --- Enable Docker CE repo ---
+   63  sudo yum install -y yum-utils
+   64  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+   65  # --- Install Docker Engine ---
+   66  sudo yum install -y docker-ce docker-ce-cli containerd.io
+   67  # --- Start and enable Docker service ---
+   68  sudo systemctl start docker
+   69  sudo systemctl enable docker
+   70  sudo systemctl status docker
+   71  cd ..
+   72  cd .
+   73  docker --version
+   74  docker login
+
+
+
